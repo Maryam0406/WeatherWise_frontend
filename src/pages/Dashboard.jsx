4a984f1a0301398f383abeae70307afcbd0fe156
MSM Shahla <maryamshahla465@gmail.com>
2026-08-21 21:25:08 +0530

    added dashboard header and user greeting

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,13 @@
-// src/pages/Dashboard.jsx
+import { Link } from 'react-router-dom';
 function Dashboard() {
-  return <h1>Dashboard Page</h1>;
-}
+  return (
+    <div className="p-8">
+      {/*Top Bar*/}
+      <div className="flex justify-between items-center mb-8 pb-4 border-b border-gray-200">
+        <h1 className="text-2xl font-bold text-gray-800">Dashboard</h1>
+        <p className="text-sm text-gray-500">
+          Hi, {mockUser.name} ({mockUser.role})
+        </p>
+      </div>
+
 export default Dashboard;
